7cacad394599ecbc867dc1641940268b04cbdb27
chrishiggins54 <97624771+chrishiggins54@users.noreply.github.com>
2023-05-11 02:17:59 +0100

    Add project files.

---
+++ CA3_s00219815/ConsoleInfo.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CA3_s00219815
+{
+    // Class to hold console information
+
+    public class ConsoleInfo
+    {
+        public int ID { get; set; }
+        public string Brand { get; set; }
+        public string Model { get; set; }
+        public string Type { get; set; }
+    }
+}
---
+++ CA3_s00219815/MainWindow.xaml.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CA3_s00219815
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+
+    {
+        public Booking CurrentBooking { get; set; }
+
+        GameConsoleDB db = new GameConsoleDB();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+        }
+
+
+        private void Window_Load(object sender, RoutedEventArgs e)
+        {
+            var cbx = from c in db.Consoles
+                      select c.Type;
+
+            cbxConsoleType.ItemsSource = cbx.ToList().Distinct();
+
+            DisplayAllConsoles();
+            DisplayAllBookings();
+        }
+
+        private void DisplayAllConsoles()
+        {
+            var query = from c in db.Consoles
+                        select c;
+            dgCustomersEx2.ItemsSource = query.ToList();
+        }
+
+        private void DisplayAllBookings()
+        {
+            var query = from b in db.Bookings
+                        select new { b.ID, b.StartDate, b.EndDate, ConsoleBrand = b.Console.Brand, ConsoleModel = b.Console.Model, ConsoleType = b.Console.Type };
+            dgCustomersEx3.ItemsSource = query.ToList();
+        }
+
+
+
+        private void ShowAvailableConsoles()
+        {
+            string selectedConsoleType = cbxConsoleType.SelectedItem as string;
+            DateTime startDate = dpStartDate.SelectedDate ?? DateTime.MinValue;
+            DateTime endDate = dpEndDate.SelectedDate ?? DateTime.MaxValue;
+
+            var query = from c in db.Consoles
+                        where c.Type.Equals(selectedConsoleType)
+                        && !c.Bookings.Any(b => (b.StartDate <= endDate && b.EndDate >= startDate))
+                        orderby c.Type descending
+                        select new ConsoleInfo
+                        {
+                            ID = c.ID,
+                            Brand = c.Brand,
+                            Model = c.Model,
+                            Type = c.Type
+                        };
+
+            dgConsolesAvailable.ItemsSource = query.ToList();
+        }
+
+
+        private void dgConsolesAvailable_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (dgConsolesAvailable.SelectedItem is ConsoleInfo selectedConsole)
+            {
+                tblkSelectedConsole.Text = $"Brand: {selectedConsole.Brand}, Model: {selectedConsole.Model}, Type: {selectedConsole.Type}";
+                CurrentBooking = new Booking { ConsoleID = selectedConsole.ID };
+            }
+        }
+
+
+
+        private void cbxConsoleType_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            ShowAvailableConsoles();
+        }
+
+        private void dpStartDate_SelectedDateChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (dpStartDate.SelectedDate.HasValue && dpEndDate.SelectedDate.HasValue)
+            {
+                if (dpStartDate.SelectedDate.Value.Date >= dpEndDate.SelectedDate.Value.Date)
+                {
+                    MessageBox.Show("Start date must be before end date.");
+                    dpStartDate.SelectedDate = null;
+                    return;
+                }
+            }
+
+            if (dpStartDate.SelectedDate.HasValue)
+            {
+                if (CurrentBooking == null)
+                    CurrentBooking = new Booking();
+
+                CurrentBooking.StartDate = dpStartDate.SelectedDate.Value;
+            }
+        }
+
+        private void dpEndDate_SelectedDateChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (dpEndDate.SelectedDate.HasValue && dpStartDate.SelectedDate.HasValue)
+            {
+                if (dpEndDate.SelectedDate.Value.Date <= dpStartDate.SelectedDate.Value.Date)
+                {
+                    MessageBox.Show("End date must be after start date.");
+                    dpEndDate.SelectedDate = null;
+                    return;
+                }
+            }
+
+            if (dpEndDate.SelectedDate.HasValue)
+            {
+                if (CurrentBooking == null)
+                    CurrentBooking = new Booking();
+
+                CurrentBooking.EndDate = dpEndDate.SelectedDate.Value;
+            }
+        }
+
+
+        private void btnBook_Click(object sender, RoutedEventArgs e)
+        {
+            if (CurrentBooking != null)
+            {
+                db.Bookings.Add(CurrentBooking);
+                db.SaveChanges();
+                DisplayAllBookings(); // refresh bookings data grid
+
+                MessageBox.Show("The console has been booked!");
+            }
+            else
+            {
+                MessageBox.Show("Please select a console and date range first.");
+            }
+        }
+
+        private void btnSearchConsoles_Click(object sender, RoutedEventArgs e)
+        {
+            ShowAvailableConsoles();
+        }
+
+
+
+    }
+}
+
